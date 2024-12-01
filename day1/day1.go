2d0d5cb8cd1d7e92f2278339003b9b9e344bc98c
brian <>
2024-12-01 05:33:04 +0000

    Day1
    FossilOrigin-Name: 8ada972bd728db8d7f8dc805ff59f264fce05c2ab050e3f53f9eb2476092ecae

--- day1/day1.go
+++ day1/day1.go
@@ -4,18 +4,45 @@ import (
 	_ "embed"
 	"fmt"
 	"github.com/baritonehands/aoc-2024-go/utils"
-	"maps"
+	"math"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 //go:embed input.txt
 var input string
 
 func main() {
-	ret := utils.PartitionFunc2([]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, func(n int) int {
-		return n / 3
-	})
-
-	for v := range ret {
-		fmt.Printf("%v\n", maps.Collect(v))
+	lines := strings.Split(input, "\n")
+	left := make([]int, len(lines))
+	right := make([]int, len(lines))
+	for i, line := range lines {
+		parts := strings.Split(line, "   ")
+		l, _ := strconv.Atoi(parts[0])
+		r, _ := strconv.Atoi(parts[1])
+		left[i] = l
+		right[i] = r
 	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+	fmt.Printf("%v\n%v\n", left, right)
+
+	sum := 0
+	for i, l := range left {
+		r := right[i]
+
+		sum += int(math.Abs(float64(l - r)))
+	}
+	println("part1", sum)
+
+	similarity := 0
+	freqs := utils.Frequencies(slices.Values(right))
+	for _, l := range left {
+		rFreq := freqs[l]
+		similarity += l * int(rFreq)
+	}
+
+	println("part2", similarity)
 }
